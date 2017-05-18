b67e30485691e4ab34ce34da3496c03a03533a26
minsuk <min6434@gmail.com>
2017-05-18 18:11:22 +0900

    [REFACTORED]: reading images by os.listdir

--- oct/camera_test/camera_test/camera_test.py
+++ oct/camera_test/camera_test/camera_test.py
@@ -1,17 +1,64 @@
-﻿import os.path
+﻿print('Importing modules...' , end='\t')
+import os
 import numpy as np
-#import sklearn
-import cv2
+from cv2 import imread 
+import seaborn as sns
+import matplotlib.pylab as plt
+import scipy.stats as sp
+print('Finished!')
 
-img_stack = np.zeros((1728,2336,100))
-dir = '.\\image\\2017_05_15_16_42_44_'
-ext = '.tif'
+def CreateIntensityProfile(height, width, strFolder):
+    "Create 1D and 2D average intensity profile of images"
+    print('Reading imges from '+strFolder+'...' , end='')
+    img_stack = np.zeros((height,width,100))
 
-# Read grayscale image and stack in 3D form
-for i in range(100): img_stack[:,:,i] = cv2.imread(dir+str(i+1).zfill(4)+ext, 0)
+    # Read grayscale image and stack in 3D form
+    for i, file in enumerate(os.listdir(strFolder)):
+        img_stack[:,:,i]=imread(os.path.join(strFolder,file), 0)
 
-intensity = np.mean(img_stack, axis=(0,1))
-#print(np.std(img_stack))
+    intensity1D = np.mean(img_stack, axis=(0,1))
+    intensity2D = np.mean(img_stack, axis=2)
+    print('Finished!')
+    return intensity1D, intensity2D
+
+height = 1728; width = 2336
+img_stack = np.zeros((height,width,100))
+curfolder = os.getcwd()
+
+# Create 2D & 1D average intensity data
+ref_path = os.path.join(curfolder,'ref')
+ref_1D, ref_2D = CreateIntensityProfile(height, width, ref_path)
+
+nobs, minmax, mean, variance = sp.describe(ref_1D)[:4]; std = np.sqrt(variance) 
+rv = sp.t(df=nobs-1, loc=mean, scale=std)
+
+# KDE plot of the intensity distribution and its estimated t pdf
+xx = np.linspace(minmax[0],minmax[1],150)
+sns.distplot(ref_1D, kde=True, rug=True);
+plt.plot(xx, rv.pdf(xx))
+plt.show()
+
+for folder in os.listdir(curfolder):
+    full_dir = os.path.join(curfolder, folder)
+    if folder != 'ref' and os.path.isdir(full_dir): 
+        intensity1D, intensity2D = CreateIntensityProfile(height, width, full_dir)
+            
+'''
+IntOutliers = 0
+for sample in intensity1D:
+    if rv.pdf(sample) < 0.99:
+        IntOutliers += 1
+
+print('Intensity outliers: ', IntOutliers)
+
+MSEOutliers = 0
+for index in range(nobs):
+    print(np.mean(np.sqrt(np.square(intensity2D-img_stack[:,:,index]))))
+    if np.mean(np.sqrt(np.square(intensity2D-img_stack[:,:,index]))) > 0.01:
+        MSEOutliers += 1
+
+print('Mean square error outliers: ', MSEOutliers)
+'''
 
 '''
 # Display sample code
