a4502e65deec2ed97cc7c867e6c38c98857f26e5
minsuk <min6434@gmail.com>
2017-05-19 16:57:49 +0900

    [CHANGED]: change outlier calculation routines from iteration to matrix calculation form

--- oct/camera_test/camera_test/camera_test.py
+++ oct/camera_test/camera_test/camera_test.py
@@ -8,57 +8,57 @@ import scipy.stats as sp
 print('Finished!')
 
 def CreateIntensityProfile(height, width, strFolder):
-    "Create 1D and 2D average intensity profile of images"
+    "Create 1D and 2D average intensity profile of images \
+     return: (intensity_1D, intensity_2D, intensity_3D) "
+
     print('Reading imges from '+strFolder+'...' , end='')
     img_stack = np.zeros((height,width,100))
 
     # Read grayscale image and stack in 3D form
-    for i, file in enumerate(os.listdir(strFolder)):
-        img_stack[:,:,i]=imread(os.path.join(strFolder,file), 0)
+    for i, file in enumerate(os.listdir(strFolder)):img_stack[:,:,i]=imread(os.path.join(strFolder,file), 0)
 
-    intensity1D = np.mean(img_stack, axis=(0,1))
-    intensity2D = np.mean(img_stack, axis=2)
     print('Finished!')
-    return intensity1D, intensity2D
+    return np.mean(img_stack, axis=(0,1)), np.mean(img_stack, axis=2), img_stack
+
+def Outliers1D(minmax, sample_1D):
+    idx = (sample_1D<minmax[0]*0.95) | (sample_1D>minmax[1]*1.05)
+    return (np.where(idx), intensity1D[idx])
+
+def Outliers2D(ref_2D, sample_3D, threshold):
+    ref_3D = np.repeat(ref_2D[:,:,np.newaxis], sample_3D.shape[2], axis=2)
+    RMS = np.sqrt(np.mean(np.square(ref_3D-sample_3D),axis=(0,1)))
+    idx = RMS>threshold
+    return (np.where(idx), RMS[idx])
 
 height = 1728; width = 2336
-img_stack = np.zeros((height,width,100))
-curfolder = os.getcwd()
 
 # Create 2D & 1D average intensity data
+curfolder = os.getcwd()
 ref_path = os.path.join(curfolder,'ref')
-ref_1D, ref_2D = CreateIntensityProfile(height, width, ref_path)
+ref_1D, ref_2D = CreateIntensityProfile(height, width, ref_path)[:2]
 
 nobs, minmax, mean, variance = sp.describe(ref_1D)[:4]; std = np.sqrt(variance) 
+print(nobs, minmax, mean, variance)
 rv = sp.t(df=nobs-1, loc=mean, scale=std)
 
 # KDE plot of the intensity distribution and its estimated t pdf
-xx = np.linspace(minmax[0],minmax[1],150)
 sns.distplot(ref_1D, kde=True, rug=True);
-plt.plot(xx, rv.pdf(xx))
-plt.show()
+#xx = np.linspace(minmax[0],minmax[1],150)
+#plt.plot(xx, rv.pdf(xx))
+#plt.show()
 
+# Find outliers in the samples
 for folder in os.listdir(curfolder):
     full_dir = os.path.join(curfolder, folder)
     if folder != 'ref' and os.path.isdir(full_dir): 
-        intensity1D, intensity2D = CreateIntensityProfile(height, width, full_dir)
-            
-'''
-IntOutliers = 0
-for sample in intensity1D:
-    if rv.pdf(sample) < 0.99:
-        IntOutliers += 1
+        intensity1D, intensity2D, img_stack = CreateIntensityProfile(height, width, full_dir)
+        print(sp.describe(intensity1D)[:4])
+        outliers_1D = Outliers1D(minmax, intensity1D); print(outliers_1D)
+        outliers_2D = Outliers2D(ref_2D, img_stack, 1); print(outliers_2D)  
+        sns.distplot(intensity1D, kde=True, rug=True);
+        
+plt.show()        
 
-print('Intensity outliers: ', IntOutliers)
-
-MSEOutliers = 0
-for index in range(nobs):
-    print(np.mean(np.sqrt(np.square(intensity2D-img_stack[:,:,index]))))
-    if np.mean(np.sqrt(np.square(intensity2D-img_stack[:,:,index]))) > 0.01:
-        MSEOutliers += 1
-
-print('Mean square error outliers: ', MSEOutliers)
-'''
 
 '''
 # Display sample code
